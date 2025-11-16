beafb510d051b0b5d7493e4babc8657b8a8ca26e
Jan <jdndiaz@addu.edu.ph>
2025-11-17 03:15:15 +0800

    fix: Update route naming

--- Modules/CustomerBooking/routes/api.php
+++ Modules/CustomerBooking/routes/api.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Modules\CustomerBooking\Http\Controllers\CustomerBookingController;
 
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('customerbookings', CustomerBookingController::class)->names('customerbooking');
+    Route::apiResource('customerbookings', CustomerBookingController::class)->names('api.customerbooking');
 });
